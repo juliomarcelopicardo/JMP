2ee453ffaeb378b9d4088f71fb5ac900a467b980
Julio Marcelo Picardo <juliomarcelopicardo@gmail.com>
2017-10-20 18:37:47 +0100

    [TEXTPARSER] added class base.

---
+++ include/text_parser.h
@@ -0,0 +1,62 @@
+/** Copyright Julio Marcelo Picardo 2017-18, all rights reserved.
+*
+*  @project JMP - A Scripting Language for Game Engines.
+*  @brief Final Project: BSc (Honours) Computer Science for Games.
+*  @supervisor Dr. Paul Parry <p.w.parry@shu.ac.uk>
+*  @author Julio Marcelo Picardo <juliomarcelopicardo@gmail.com>
+*                                <b7026027@my.shu.ac.uk>
+*/
+
+#ifndef __JMP_TEXTPARSER_H__
+#define __JMP_TEXTPARSER_H__ 1
+
+#include "types.h"
+
+namespace JMP {
+
+/// Text parser used to save all the tokens from .jmp files.
+class TextParser {
+
+ public: 
+
+/*******************************************************************************          
+***                             PUBLIC METHODS                               ***
+*******************************************************************************/
+
+  /// Default class constructor.
+  TextParser();
+  /// Default class destructor.
+  ~TextParser();
+
+
+
+/*******************************************************************************
+***                           PUBLIC ATTRIBUTES                              ***
+*******************************************************************************/
+
+
+
+
+
+ private:
+
+/*******************************************************************************
+***                            PRIVATE METHODS                               ***
+*******************************************************************************/
+
+
+  /// Default copy constructor.
+  TextParser(const TextParser& copy);
+  /// Assignment operator.
+  TextParser& operator=(const TextParser& copy);
+
+/*******************************************************************************
+***                          PRIVATE ATTRIBUTES                              ***
+*******************************************************************************/
+
+
+
+}; /* TextParser */
+}; /* JMP */
+
+#endif /* __JMP_TEXTPARSER_H__ */
---
+++ src/text_parser.cc
@@ -0,0 +1,21 @@
+/** Copyright Julio Marcelo Picardo 2017-18, all rights reserved.
+*
+*  @project JMP - A Scripting Language for Game Engines.
+*  @brief Final Project: BSc (Honours) Computer Science for Games.
+*  @supervisor Dr. Paul Parry <p.w.parry@shu.ac.uk>
+*  @author Julio Marcelo Picardo <juliomarcelopicardo@gmail.com>
+*                                <b7026027@my.shu.ac.uk>
+*/
+
+#include "text_parser.h"
+
+namespace JMP {
+
+TextParser::TextParser() {}
+
+TextParser::~TextParser() {}
+
+
+
+
+}; /* JMP */
