a7b4f80b65e31c9cb125fb00f7f286eb4ddca492
Julio Marcelo Picardo <juliomarcelopicardo@gmail.com>
2017-10-20 18:19:30 +0100

    [JMP] Main file to make all the includes.

---
+++ include/jmp.h
@@ -0,0 +1,15 @@
+/** Copyright Julio Marcelo Picardo 2017-18, all rights reserved.
+*
+*  @project JMP - A Scripting Language for Game Engines.
+*  @brief Final Project: BSc (Honours) Computer Science for Games.
+*  @supervisor Dr. Paul Parry <p.w.parry@shu.ac.uk>
+*  @author Julio Marcelo Picardo <juliomarcelopicardo@gmail.com>
+*                                <b7026027@my.shu.ac.uk>
+*/
+
+#ifndef __JMP_H__
+#define __JMP_H__ 1
+
+#include <types.h>
+
+#endif /* __JMP_H__ */
