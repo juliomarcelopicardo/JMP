3bb9fdcd2db325978f4ae27485296d37713b8328
Julio Marcelo Picardo <juliomarcelopicardo@gmail.com>
2017-10-20 18:42:35 +0100

    [MAIN] Basic main for testing

---
+++ src/main.cc
@@ -0,0 +1,20 @@
+/** Copyright Julio Marcelo Picardo 2017-18, all rights reserved.
+*
+*  @project JMP - A Scripting Language for Game Engines.
+*  @brief Final Project: BSc (Honours) Computer Science for Games.
+*  @supervisor Dr. Paul Parry <p.w.parry@shu.ac.uk>
+*  @author Julio Marcelo Picardo <juliomarcelopicardo@gmail.com>
+*                                <b7026027@my.shu.ac.uk>
+*/
+
+#include "jmp.h"
+#include <Windows.h>
+
+JMP::int32 main() {
+  OutputDebugString("\n Starting application... \n\n\n\n\n");
+
+
+  OutputDebugString("\n Ending application... \n\n");
+  return 0;
+}
+
