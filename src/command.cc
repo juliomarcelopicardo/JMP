b4499a8d2db4f3ce0fcfe353a60c04a4de2e3e44
Julio Marcelo Picardo <juliomarcelopicardo@gmail.com>
2018-01-21 17:19:40 +0000

    [COMMAND] Execute Division method.

--- src/command.cc
+++ src/command.cc
@@ -102,7 +102,12 @@ Report Command::executeMultiply(Machine* machine, int32& next_cmd_id) {
   return kReport_NoErrors;
 }
 Report Command::executeDivision(Machine* machine, int32& next_cmd_id) {
-  return Report();
+  // Take the last members pushed in the stack
+  Value second = machine->getAndRemoveTheLastAddedStackValue();
+  Value first = machine->getAndRemoveTheLastAddedStackValue();
+  machine->addValueToTheStack(first / second);
+  next_cmd_id++; // Jump to the next step.
+  return kReport_NoErrors;
 }
 Report Command::executePower(Machine* machine, int32& next_cmd_id) {
   return Report();
