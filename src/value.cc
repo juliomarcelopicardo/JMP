58e0ea7557d956ba1f4a209a3bfa70690a385af8
Julio Marcelo Picardo <juliomarcelopicardo@gmail.com>
2018-01-21 17:14:42 +0000

    [VALUE] Operator*

--- src/value.cc
+++ src/value.cc
@@ -145,4 +145,29 @@ Value operator-(const Value& a, const Value& b) {
   return Value();
 }
 
+Value operator*(const Value& a, const Value& b) {
+  // integer * integer = integer 
+  if (a.type_ == kValueType_Integer && b.type_ == kValueType_Integer) {
+    return{ a.integer_ * b.integer_ };
+  }
+
+  // float * float = float 
+  if (a.type_ == kValueType_Float && b.type_ == kValueType_Float) {
+    return{ a.float_ * b.float_ };
+  }
+
+  // integer * float = float
+  if (a.type_ == kValueType_Integer && b.type_ == kValueType_Float) {
+    return{ (float32)a.integer_ * b.float_ };
+  }
+
+  // float * integer = float
+  if (a.type_ == kValueType_Float && b.type_ == kValueType_Integer) {
+    return{ a.float_ * (float32)b.integer_ };
+  }
+
+  ReportWarning("Value Substraction Operation Failed.");
+  return Value();
+}
+
 }; /* JMP */
